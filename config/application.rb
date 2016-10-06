92f8c5a3ae1c80bb4f63cff87b8cf247908d8923
Ezzy Sriram <ezzy.sriram@gmail.com>
2016-10-06 13:25:51 -0700

    Iter 0 final push

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module AlzAboutMe
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.web_console.development_only = false
   end
 end
