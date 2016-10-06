87984ed19b832696f04ec186757e70cf9a9d6124
Ezzy Sriram <ezzy.sriram@gmail.com>
2016-10-06 15:12:40 -0700

    added welcome page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
