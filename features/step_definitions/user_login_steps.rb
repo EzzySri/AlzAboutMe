9d4b47ef13a631804b262231fdb9b0430609c043
hbonilla2 <hbonilla@berkeley.edu>
2016-10-18 05:47:31 +0000

    user_login steps are pending

---
+++ features/step_definitions/user_login_steps.rb
@@ -0,0 +1,22 @@
+Given(/^I have an account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I'm on the home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I type in my username$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I type in my password$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+Then(/^I should be logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
