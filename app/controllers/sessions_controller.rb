0d7271879bb5f7f09bef8c1a3934f47ef7c56785
Kadie Jaffe <kadiejaffe@gmail.com>
2016-10-19 21:12:04 +0000

    updated session methods to facilitate login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,26 @@
 class SessionsController < ApplicationController
-  def login
+  def new
   end
 
-  def logout
+  def create
+    user = User.find_by_username(params[:username])
+    # If the user exists AND the password entered is correct.
+    
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to '/'
+      flash[:notice] = 'Login Successful'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+      flash[:notice] = 'Login Failed'
+    end
   end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end 
 end
