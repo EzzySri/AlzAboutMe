2dbd7c6b8de50fc93e4d900f29909d12fa452d96
wongdaniel8 <wongdaniel8@yahoo.com>
2016-10-19 04:30:30 +0000

    flash edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
   def homefeed
     if params["user"] != nil and params["user"]["memory"] != nil
-      flash[:notice] = "memory was successfully created."
+      flash[:notice] = "memory was successfully edited."
       session[:recorded_memory] = params["user"]["memory"]
     end
   end
