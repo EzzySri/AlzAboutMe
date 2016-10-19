ffad6737bc2399eba3cd3199aad570cfa0dbb39a
wongdaniel8 <wongdaniel8@yahoo.com>
2016-10-19 04:00:20 +0000

    fixed memory bank to input and keep memory, flash added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def new
   end
   def homefeed
+    if params["user"] != nil and params["user"]["memory"] != nil
+      flash[:notice] = "memory was successfully created."
+      session[:recorded_memory] = params["user"]["memory"]
+    end
   end
   # def show
   # end
