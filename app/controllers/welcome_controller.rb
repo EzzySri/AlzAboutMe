9488c404fee8a7f131a10b83cb295d38cbb22817
Kadie Jaffe <kadiejaffe@gmail.com>
2016-10-20 22:55:44 +0000

    welcome header displays name if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
 end
