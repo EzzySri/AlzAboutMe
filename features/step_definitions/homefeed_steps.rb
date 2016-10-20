0e8699d9687dde6f68d25180134fcc0edf01f46e
hbonilla2 <hbonilla@berkeley.edu>
2016-10-20 20:29:39 +0000

    DRYed out SignUp step defs, renamed ids

--- features/step_definitions/homefeed_steps.rb
+++ features/step_definitions/homefeed_steps.rb
@@ -1,7 +1,7 @@
-Given(/^I am on the homefeed page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-And /I fill in the memory box with "my memory here"/ do
+# Given(/^I am on the homefeed page$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+And /I fill in memory box with "my memory here"/ do
     pending
 end
 Then /I should see "my memory here"/ do
@@ -10,8 +10,3 @@ end
 Then /I should see "memory added"/ do
     pending
 end
-
-
-# Then(/^I should be on the "([^"]*)" page$/) do |arg1|
-#     visit('/users/' + arg1)
-# end
