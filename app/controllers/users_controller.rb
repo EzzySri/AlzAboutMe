a0f57fa69dfe0b4eed25c25b6e22b4686615fb7b
Filip <filip.mirdita@gmail.com>
2016-10-20 23:47:52 +0000

    bootstrap update, convert application.html to haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
         session[:user_id] = @user.id
         redirect_to '/'
       else 
-        redirect_to users_path
+        flash[:notice] = "Invalid Sign Up Information, Please Try Again"
+        redirect_to users_signup_path
       end
   end
 
