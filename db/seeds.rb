23a2ea9a2ec64506f51f152da1be7a5db5dd29f0
Ezzy Sriram <ezzy.sriram@gmail.com>
2016-11-02 12:31:16 -0700

    added multiple choice question functionality to memory cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 memories = [ {:question => "Question here", :answer => "answer here", :question_type => "text", :completed => false},
              {:question => "Question 2 here", :answer => "answer 2 here", :question_type => "text", :completed => false},
+             {:question => "Multiple-choice question here", :answer => "answer here", :question_type => "multiple_choice", :question_choices => "red|blue|yellow", :completed => false}
 ]
+
 
 memories.each do |mem|
     MemoryCard.create!(mem)
