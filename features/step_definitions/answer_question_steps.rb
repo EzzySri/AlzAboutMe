34cc5cdd2f673dc283148df540942a1291950af5
Kadie Jaffe <kadiejaffe@gmail.com>
2016-11-03 03:53:32 +0000

    answer_question steps

--- features/step_definitions/answer_question_steps.rb
+++ features/step_definitions/answer_question_steps.rb
@@ -1,11 +1,11 @@
 
 Given(/^I click on a fill in memory card$/) do
   # TODO: need to find a way to select short answer memcard in general 
-  visit('/memory_cards/3/edit')
+  visit('/memory_cards/1/edit')
 end
 
 Given(/^I click on a multiple choice memory card$/) do
   # TODO: need to find a way to select mult choice answer memcard in general 
-  visit('/memory_cards/5/edit')
+  visit('/memory_cards/2/edit')
 end
 
