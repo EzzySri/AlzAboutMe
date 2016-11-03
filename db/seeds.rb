54d49f3d86bf17e42833f433e6f46423bf182611
wongdaniel8 <wongdaniel8@yahoo.com>
2016-11-03 01:29:59 +0000

    fixed everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-memories = [ {:question => "Question here", :answer => "answer here", :question_type => "text", :completed => false},
-             {:question => "Question 2 here", :answer => "answer 2 here", :question_type => "text", :completed => false},
-             {:question => "Multiple-choice question here", :answer => "answer here", :question_type => "multiple_choice", :question_choices => "red|blue|yellow", :completed => false}
+memories = [ {:question => "Question here", :answer => "answer here", :question_type => "text", :completed => false, :editing => false},
+             {:question => "Question 2 here", :answer => "answer 2 here", :question_type => "text", :completed => false, :editing => false},
+             {:question => "Multiple-choice question here", :answer => "answer here", :question_type => "multiple_choice", :question_choices => "red|blue|yellow", :completed => false, :editing => false}
 ]
 
 
