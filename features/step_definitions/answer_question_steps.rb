b0a8bdea31bd760eb0ae5eeabaf74f890e2f7a16
Kadie Jaffe <kadiejaffe@gmail.com>
2016-11-03 05:51:18 +0000

    answer_step

--- features/step_definitions/answer_question_steps.rb
+++ features/step_definitions/answer_question_steps.rb
@@ -2,8 +2,9 @@
 
 Given(/^I click on a fill in memory card$/) do
   # TODO: need to find a way to select short answer memcard in general 
-  card = FactoryGirl.build(:memory_cards, question_type: "text")
-  page.find("#short_answer_card", :visible => false).click
+  # card = FactoryGirl.build(:memory_cards, question_type: "text")
+  # page.find("#short_answer_card", :visible => false).click
+  click_on("Question here")
 end
 
 Given(/^I click on a multiple choice memory card$/) do
