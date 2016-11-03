d721d71777fc655614d85fcbe834f5fcf715c14f
Kadie Jaffe <kadiejaffe@gmail.com>
2016-11-03 04:24:30 +0000

    fixing haml to add ids

--- features/step_definitions/answer_question_steps.rb
+++ features/step_definitions/answer_question_steps.rb
@@ -1,11 +1,11 @@
 
 Given(/^I click on a fill in memory card$/) do
   # TODO: need to find a way to select short answer memcard in general 
-  page.find_by_id('short_answer_card').click
+  find_by_id(short_answer_card).click
 end
 
 Given(/^I click on a multiple choice memory card$/) do
   # TODO: need to find a way to select mult choice answer memcard in general 
-  page.find_by_id('mult_answer_card').click
+  find_by_id('mult_answer_card').click
 end
 
