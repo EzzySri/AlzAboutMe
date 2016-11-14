09226119e5efa6f2a99e4b069471fb02b5457443
Filip <filip.mirdita@gmail.com>
2016-11-14 17:31:36 +0000

    database adjustment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-memories = [ {:question => "Where were you born?", :answer => "answer here", :question_type => "text", :completed => false, :editing => false, :category => "Personal"},
-             {:question => "What's your astrological sign?", :answer => "answer 2 here", :question_type => "text", :completed => false, :editing => false, :category => "Personal"},
-             {:question => "What's your favorite color?", :answer => "answer here", :question_type => "multiple_choice", :question_choices => "red|blue|yellow", :completed => false, :editing => false, :category => "Favorites"}
+memories = [ {:question => "Where were you born?", :question_type => "text", :completed => false, :editing => false, :category => "Personal"},
+             {:question => "What's your astrological sign?", :question_type => "text", :completed => false, :editing => false, :category => "Personal"},
+             {:question => "What's your favorite color?", :question_type => "multiple_choice", :question_choices => "Red|Blue|Yellow|Green", :completed => false, :editing => false, :category => "Favorites"}
 ]
 
 memories.each do |mem|
