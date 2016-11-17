62dc2f14f2dc557f7f43a36eddaff617d0a89420
wongdaniel8 <wongdaniel8@yahoo.com>
2016-11-17 06:17:20 +0000

    additional rspec mem cards added

--- spec/memory_cards_spec.rb
+++ spec/memory_cards_spec.rb
@@ -32,4 +32,49 @@ RSpec.describe MemoryCardsController, type: :controller do
         end
     end
     
+    describe 'PUT update' do
+        it 'PUT :id/update updates memory card' do
+           @memCard = MemoryCard.create(:id => 1, :user_id => 123, :question => "What is your name?", :answer => "Zack", :editing => false)
+           xhr :put, 'update', {:id => @memCard, :user => { :memory => "New answer to memory card question" }}
+           @memCard.reload
+           expect(@memCard.answer).to eql("New answer to memory card question" )
+        end
+
+        it 'PUT :id/update responds with empty JS response' do
+           @memCard = MemoryCard.create(:id => 1, :user_id => 123, :question => "What is your name?", :answer => "Zack", :editing => false)
+           xhr :put, 'update', :id => @memCard.id
+           response.content_type.should == Mime::JS
+        end
+    end
+
+    describe 'PUT edit' do
+        it 'PUT :id/edit flips memory card into edit mode' do
+           @memCard = MemoryCard.create(:id => 1, :user_id => 123, :question => "What is your name?", :answer => "Zack", :editing => false)
+           xhr :put, 'edit', :id => @memCard
+           @memCard.reload
+           expect(@memCard.editing).to eql(true)
+        end
+
+        it 'PUT :id/edit responds with empty JS response' do
+           @memCard = MemoryCard.create(:id => 1, :user_id => 123, :question => "What is your name?", :answer => "Zack", :editing => false)
+           xhr :put, 'edit', :id => @memCard.id
+           response.content_type.should == Mime::JS
+        end
+    end
+
+    describe 'PUT exit' do
+        it 'PUT :id/cancel flips memory card out of edit mode' do
+           @memCard = MemoryCard.create(:id => 1, :user_id => 123, :question => "What is your name?", :answer => "Zack", :editing => true)
+           xhr :put, 'exit', :id => @memCard
+           @memCard.reload
+           expect(@memCard.editing).to eql(false)
+        end
+
+        it 'PUT :id/exit responds with empty JS response' do
+           @memCard = MemoryCard.create(:id => 1, :user_id => 123, :question => "What is your name?", :answer => "Zack", :editing => false)
+           xhr :put, 'exit', :id => @memCard.id
+           response.content_type.should == Mime::JS
+        end
+    end
+    
 end
