863552f3aa93092ae8d60b39b6d7dd25e641477b
Kadie Jaffe <kadiejaffe@gmail.com>
2016-11-17 23:49:57 +0000

    including controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
+  include CanCan::ControllerAdditions
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include CanCan::ControllerAdditions
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
