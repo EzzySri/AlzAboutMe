64b82e6dbf59c5240bfdea56d15342e2c10df196
wongdaniel8 <wongdaniel8@yahoo.com>
2016-11-17 02:26:12 +0000

    commenting out unused code

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
    describe 'GET index' do
        
-        it 'shows index page' do
-            # get :index
-            # expect(response).to render_template 'index'
-            expect(2).to eql(2)
-        end
+        # it 'shows index page' do
+        #     # get :index
+        #     # expect(response).to render_template 'index'
+        #     expect(2).to eql(2)
+        # end
         
-        it 'handles new user' do
-            get :new
-            expect(@user).not_to eql(nil)
-        end
+        # it 'handles new user' do
+        #     get :new
+        #     expect(@user).not_to eql(nil)
+        # end
        
        # it 'editing card' do
        #     @memCard = MemoryCard.create(:user_id => 123, :question => "What is your name?", :answer => "Zack")
