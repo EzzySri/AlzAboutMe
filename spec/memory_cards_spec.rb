462b9b614ef5891d7eac255d100664c0f8edf748
Zack Tobar <zacktobar13@gmail.com>
2016-11-17 01:17:24 +0000

    working on testing share

--- spec/memory_cards_spec.rb
+++ spec/memory_cards_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe MemoryCardsController, type: :controller do
     describe 'GET edit' do
         
         it 'shows index page' do
-            expect(2).to eql(3)
+            @memCard = MemoryCard.create(:user_id => 123, :question => "What is your name?", :answer => "Zack")
+            @sharer = User.create(:username => "notBob", :first_name => "notBob", :id => 123)
+            @sharee = User.create(:username => "bob", :first_name => "bob", :id => 234)
+            put :share, :id => @memCard, :user => {:share => "bob"}
+            expect(response).to render_template 'memory_cards_path'
             # get :edit
             # expect(response).to render_template 'index'
         end
