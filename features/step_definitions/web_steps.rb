8faeb6a2189aacda5c0c6a3c6a552c6a1f10a62a
hbonilla2 <hbonilla@berkeley.edu>
2016-11-20 21:16:02 +0000

    branch creation

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -143,11 +143,7 @@ end
  end
 
  Then (/^(?:|I )should not see "([^"]*)"$/) do |text|
-   if page.respond_to? :should
-     page.should have_no_content(text)
-   else
-     assert page.has_no_content?(text)
-   end
+   expect(page).to_not have_content(text)
  end
 
  Then (/^(?:|I )should not see \/([^\/]*)\/$/) do |regexp|
